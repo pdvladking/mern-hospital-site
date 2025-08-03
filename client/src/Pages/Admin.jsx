c55b9e15f749cf96196e45a6a2dfa4e44955b06a
pdvladking <pdvladking@gmail.com>
2025-08-03 09:23:16 +0545

    Update axios baseURL for production

--- client/src/Pages/Admin.jsx
+++ client/src/Pages/Admin.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import AddDoctorForm from '../Components/AddDoctorForm';
+import AddDoctorForm from '../components/AddDoctorForm';
 import AppointmentsTable from '../components/AppointmentsTable';
 
 const Admin = () => {
