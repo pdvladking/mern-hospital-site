d3e823f2cfb6a03194196d58418615d4f937c178
pdvladking <pdvladking@gmail.com>
2025-08-03 09:19:03 +0545

    Update axios baseURL for production

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://mern-hospital-site-3.onrender.com/api',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
